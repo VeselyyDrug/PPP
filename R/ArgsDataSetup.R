a9704182682a4a12ca638c37bc8285a2a7e689b6
probot <will@probert.co.nz>
2012-11-07 16:37:15 +1000

    Changed = to <- in variable definition

--- R/ArgsDataSetup.R
+++ R/ArgsDataSetup.R
@@ -11,38 +11,38 @@ functions.dir <- path.expand("~/PPP/R") # directory all functions are stored in
 data<-"TESTMASTER_SPECIES_PRESCRIPTIONS_30May_2012.mdb" # name of the database
 
 # polygon thresholds
-poly.size=1 # threshold for polgon slither deletion (hectares)
-poly.percent=0.5 # threshold for polygon slither deletion (proportion of original size)
+poly.size <- 1 # threshold for polgon slither deletion (hectares)
+poly.percent <- 0.5 # threshold for polygon slither deletion (proportion of original size)
 
 # strip projects settings
-strip.projects=TRUE # set to TRUE if you wish to limit the number of projects prioritised by either number and/or taxa type
-select.projects=TRUE # set to true to limit projects to only those in project.list
+strip.projects <- TRUE # set to TRUE if you wish to limit the number of projects prioritised by either number and/or taxa type
+select.projects <- TRUE # set to true to limit projects to only those in project.list
 #project.list<-as.integer(unlist(read.csv("ID.csv",header=FALSE))) # list of species IDs to select
-project.list=c(14083,14003,13152,13074,2050,2032,2024)
-projects=20 # the number of projects prioritised if strip.projects=TRUE (from the order projects are listed in R_benefits)
-select.taxa=FALSE # set to TRUE to enable selection of one taxa type only for prioritisation
+project.list <- c(14083,14003,13152,13074,2050,2032,2024)
+projects <- 20 # the number of projects prioritised if strip.projects=TRUE (from the order projects are listed in R_benefits)
+select.taxa <- FALSE # set to TRUE to enable selection of one taxa type only for prioritisation
 taxa="Vascular Plant" # if select.taxa is TRUE, then set to taxa text
-set.benefit=FALSE # set to TRUE to set benefit 
-new.benefit=100 # if set.benefit is TRUE then set to 0-100 to change benefit values
-set.success=FALSE # set to TRUE to set benefit
-new.success=100 # if set.success is TRUE then set to 0-100 to change success values
+set.benefit <- FALSE # set to TRUE to set benefit 
+new.benefit <- 100 # if set.benefit is TRUE then set to 0-100 to change benefit values
+set.success <- FALSE # set to TRUE to set benefit
+new.success <- 100 # if set.success is TRUE then set to 0-100 to change success values
 
 # overlaps and output monitoring
-overlap.indicator=TRUE # set to TRUE if you want overlaps to be calculated
-output.monitoring=TRUE # set to TRUE to include output monitoring data
+overlap.indicator <- TRUE # set to TRUE if you want overlaps to be calculated
+output.monitoring <- TRUE # set to TRUE to include output monitoring data
 
 # prioritisation settings
-run.prioritisation=TRUE # TRUE - prioritisation will run immediately, FALSE - csv files will be written to directory
-set_budget=1919263 # budget limit for prioritisation
-multiplier= 1
-weighting=1
-discount.rate = 0.01
-T = 50
-stop.iteration = Inf
+run.prioritisation <- TRUE # TRUE - prioritisation will run immediately, FALSE - csv files will be written to directory
+set_budget <- 1919263 # budget limit for prioritisation
+multiplier <- 1
+weighting <- 1
+discount.rate <- 0.01
+T <- 50
+stop.iteration <- Inf
 
 # summary info
-summary=TRUE # when set to TRUE the function summary.PPP will be run after Priortisation
-prefix_text=as.character(Sys.Date()) # if set to Sys.Date() the prefix to each summary file will be today's date
+summary <- TRUE # when set to TRUE the function summary.PPP will be run after Priortisation
+prefix_text <- as.character(Sys.Date()) # if set to Sys.Date() the prefix to each summary file will be today's date
 
 # sourcing setup functions
 source(file.path(functions.dir,'DataSetup.R'))
